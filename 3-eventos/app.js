22f2d6312ea6f6f5cd9e1f8442cb8fe9c8bdd016
Hernandez13555 <168384717+Hernandez13555@users.noreply.github.com>
2025-04-25 10:22:22 -0500

    DOM

--- 3-eventos/app.js
+++ 3-eventos/app.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 import { Validar } from "./modulo";
 
 Validar;
 
-=======
-//Variables
 >>>>>>> 312ebf1d3b1286ce1a789e0f30bfae22dd187f9f
 const formulario = document.querySelector("form");
 const nombre = document.querySelector("[name=nombre]");
@@ -13,7 +10,6 @@ const telefono = document.querySelector("[name=telefono]");
 const documento = document.querySelector("[name=documento]");
 const usuario = document.querySelector("[name=usuario]");
 const contrasena = document.querySelector("[name=contrasena]");
-<<<<<<< HEAD
 const politicas = document.querySelector("[name=politicas]");
 const boton = document.querySelector("[name=btn_validar]");
 const usuarios = document.querySelector("[name=usuarios]");
@@ -163,49 +159,5 @@ formulario.addEventListener("submit", Validar);
 // documento.addEventListener("blur", limpiar);
 // usuario.addEventListener("blur", limpiar);
 // contrasena.addEventListener("blur", limpiar);
-=======
 // const btn = document.querySelector("#btn_validar");
-//Funciones
 
-const validar = (event) => {
-  const valores = [nombre, apellido, telefono, documento, usuario, contrasena];
-  const valoresString = [
-    "nombre",
-    "apellido",
-    "telefono",
-    "documento",
-    "usuario",
-    "contraseña",
-  ];
-  event.preventDefault();
-  let mensaje = "Ingrese correctamente: ";
-  for (let x = 0; x < valores.length; x++) {
-    if (valores[x].value === "") {
-      if (x > 0) mensaje += ", " + valoresString[x];
-      else mensaje += " " + valoresString[x];
-      valores[x].focus();
-    }
-  }
-  if (mensaje != "Ingrese correctamente: ") alert(mensaje);
-};
-const letras = (event) => {
-  const regexp = /^[a-zA-Z]$/;
-  if (!regexp.test(event.key)) {
-    event.preventDefault();
-  }
-};
-const numeros = (event) => {
-  if (!/^\d$/.test(event.key)) {
-    event.preventDefault();
-  }
-  if (event.target.value.length >= 10) {
-    event.preventDefault();
-  }
-};
-//Eventos
-formulario.addEventListener("submit", validar);
-nombre.addEventListener("keydown", letras);
-apellido.addEventListener("keydown", letras);
-telefono.addEventListener("keydown", numeros);
-documento.addEventListener("keydown", numeros);
->>>>>>> 312ebf1d3b1286ce1a789e0f30bfae22dd187f9f
