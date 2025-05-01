41ca029f7a4f1056dda336031b937dc2c647db64
Hernandez13555 <168384717+Hernandez13555@users.noreply.github.com>
2025-05-01 18:23:56 -0500

    DOM

--- 3-eventos/modulo.js
+++ 3-eventos/modulo.js
@@ -1,4 +1,24 @@
-export const Validar = (e) => {
-  e.preventDefaul();
-  console.log(e.target.children);
-};
+export const esValido = (e) =>
+{
+  e.preventDefault()
+  // console.log(e.target.children);
+  const campos = [...e.target].filter((elemento) => {
+    return elemento.hasAttribute('required');
+  });
+  campos.forEach(campo => {
+      switch (campo.tagName) {
+        case "INPUT":
+          console.log(campo.type);
+          break;
+        case "SELECT":
+          console.log(campo.type);
+          break;
+        default:
+          break;
+      }
+    if (campo.value.trim() === "")
+    {
+      campo.classList.add("pruebaError");
+    }
+  })
+}
